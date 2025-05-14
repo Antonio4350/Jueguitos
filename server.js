ce20942d1e7cc1c00cababd08ce5aee251230fb6
Antonio4350 <antoniocruzvalletto00@gmail.com>
2025-05-14 18:07:20 -0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");//utilizamos el express y el path para el serv
+const PORT =3000;
+
+app.use(express.static(path.join(__dirname, "public")));//carpeta estatica publica donde vamos a estar trabajando
+// rutas para enviar archivos html segun la url 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','index.html'));//direccion del index
+});
+app.get("/ej1", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','ej1.html'));//direccion de las paginas
+});
+app.get("/ej2", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','ej2.html'));
+});
+app.get("/ej3", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','ej3.html'));
+});
+
+// inicia el servidor en el puerto definido
+app.listen(3000, () => {//puerto
+  console.log("http://localhost:"+PORT); // muestra url en consola
+});
