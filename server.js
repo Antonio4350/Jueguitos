cdb566f800ec552ac1223e42e586408fdea786fa
Antonio4350 <antoniocruzvalletto00@gmail.com>
2025-05-14 18:59:04 -0300

    server, index
    actualice el server que no funcionaba bien y el index

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");//utilizamos el express y el path para el serv
-const PORT =3000;
+const PORT =8530;
 
 app.use(express.static(path.join(__dirname, "public")));//carpeta estatica publica donde vamos a estar trabajando
 // rutas para enviar archivos html segun la url 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public','paginas','index.html'));//direccion del index
 });
-app.get("/juego1", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','paginas','ej1.html'));//direccion de las paginas
+app.get("/simondice", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','juego1.html'));//direccion de las paginas
 });
-app.get("/juego2", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','paginas','ej2.html'));
+app.get("/tateti", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','juego2.html'));
 });
-app.get("/juego3", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','paginas','ej3.html'));
+app.get("/piedrapapelotijeras", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','paginas','juego3.html'));
 });
 
 // inicia el servidor en el puerto definido
-app.listen(3000, () => {//puerto
+app.listen(PORT, () => {//puerto
   console.log("http://localhost:"+PORT); // muestra url en consola
 });
