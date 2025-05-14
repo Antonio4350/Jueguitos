7d6be219a9106b2080bbcafdc9ca75861904907b
ValdezJuanIgnacio <valdezjuanignacio200789@gmail.com>
2025-05-14 19:30:47 -0300

    prueba

---
+++ public/scripts/juego1.js
@@ -0,0 +1,39 @@
+var entrar=true;
+var jugar=true;
+while(entrar==true){
+    orden= newArray();
+    orden.pop();
+    while(jugar==true){
+        numusuario=newArray();
+        var na= numeroRandom(1,4);
+        function numeroRandom(min,max){
+            orden.push(Math.floor(Math.random()* (max-min+1))+min);
+        }
+        for(let i=0;i>orden.lenght;i++)
+        {
+            if(orden[i]==1)
+            {   
+                const color= document.getElementById
+
+            }
+            if(orde[2]==1)
+            {
+                
+            }
+            if(orde[3]==1)
+            {
+                
+            }
+            if(orde[4]==1)
+            {
+                
+            }
+        }
+        console.log(orden);
+        if(orden!==numusuario)jugar==false;
+    }
+    function seguirJugando(){
+        if(cartel==false)entrar==false;
+            else if(cartel==true)jugar==true;
+    }
+}
